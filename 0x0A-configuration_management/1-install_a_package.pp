20a681d6954da5e8b510be0b122bdf8807a8ea12
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-14 05:34:46 -0700

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
-
+# Install puppet-lint 2.1.1 with Puppet
 package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+  ensure   => '2.1.1',
+  provider => gem,
 }
