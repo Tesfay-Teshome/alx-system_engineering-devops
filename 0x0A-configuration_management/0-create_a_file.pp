b9176d8089c0f0861d621da63e81cbf5b5daf183
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-14 05:08:47 -0700

    create puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Creating puppet file in /tmp
+# Create a file in /tmp
 
-file {'school':
-  ensure => 'present',
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { 'school':
+  ensure  => 'present',
   content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
