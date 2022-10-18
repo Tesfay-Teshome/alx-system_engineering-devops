8b96099ce1487582725bdeb4abd94964f64935b6
Tesfay-Teshome <tesfix2017@gmail.com>
2022-10-18 01:13:10 -0700

    install nginx puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,35 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
+# Manifest that configures NGINX
+exec { 'exec_0':
+  command => 'sudo sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://colbatanplc.com;',
+exec { 'exec_1':
+  require => Exec['exec_0'],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+exec { 'exec_2':
+  require => Exec['exec_1'],
+  command => 'echo "Hello World" | sudo tee /var/www/html/index.nginx-debian.html',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'exec_3':
+  require     => Exec['exec_2'],
+  environment => ['GG=google.com permanent'],
+  command     => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me $GG;/" /etc/nginx/sites-enabled/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
+}
+
+exec { 'exec_4':
+  require => Exec['exec_3'],
+  command => 'sudo service nginx start',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
